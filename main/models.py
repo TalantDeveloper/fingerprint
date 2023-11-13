373ed128f159eb19bbdedbca8122ddc3de88bdb5
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2023-11-13 16:46:45 +0500

    13.11.2023
    16:46

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Name")
+    icon = models.ImageField(upload_to="./category", default="./category/1234.jpg")
+
+
+class Customer(models.Model):
+    """
+    Account Management
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(upload_to='./user/', default='./user/538642.png')
+
+    def __str__(self):
+        return self.user.username
+
+
